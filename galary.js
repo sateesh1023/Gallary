02701c5fd4dabab7088bc9d29c97b928f476d113
sateesh1023 <137385257+sateesh1023@users.noreply.github.com>
2023-06-23 11:28:45 +0530

    Add files via upload

---
+++ galary.js
@@ -0,0 +1,12 @@
+var allimg=document.images
+console.log(allimg)
+
+for (i=0;i<allimg.length;i++)
+ {
+allimg[i].src=`./photos/${i}.jpg`
+allimg[i].addEventListener("click",function()
+         {
+          document.getElementById("bigimg").style.backgroundImage=`url(${this.src})`
+         console.log(this.src)
+         })
+}
